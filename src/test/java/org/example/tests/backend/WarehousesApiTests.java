cee98298c091240381d1cb753d01fa92bef9b880
Elena Voevodkina <voevodkina@hotmail.com>
2025-04-04 22:44:38 +0100

    Added new testcases for Warehouse

--- src/test/java/org/example/tests/backend/WarehousesApiTests.java
+++ src/test/java/org/example/tests/backend/WarehousesApiTests.java
@@ -8,7 +8,11 @@ import org.example.tests.BaseTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class WarehousesApiTests extends BaseTest {
 
@@ -29,28 +33,36 @@ public class WarehousesApiTests extends BaseTest {
         assertEquals(422, warehouseDuplicate.statusCode(), "The warehouse at the same location couldn't be created.");
     }
 
-//    @Test
-//    @DisplayName("Retrieving all Warehouses")
-//    void retrievingWarehouses() {
-//        Response response = warehousesServicesAPI.retrievingWarehouses(accessToken);
-//
-//        assertEquals(200, response.statusCode(), "There is error in the process of retrieving");
-//        assertNotNull(response.getBody(), "Expected a non-null body");
-//        List<Map<String, Object>> warehouses = response.jsonPath().getList("$");
-//        assertNotNull(warehouses, "The response is empty");
-//
-//        if(warehouses.isEmpty()) {
-//            System.out.println("The list [] is expected in the first retrieval when no data exists in the database, before the warehouses are created.");
-//        }
-//
-//        Set<String> warehouseId = new HashSet<>();
-//        Set<String> location = new HashSet<>();
-//
-//        for (Map<String, Object> warehouse: warehouses) {
-//
-//
-//
-//        }
-//
-//    }
+    @Test
+    @DisplayName("Retrieving all Warehouses")
+    void retrievingWarehouses() {
+        Response response = warehousesServicesAPI.retrievingWarehouses(accessToken);
+
+        assertEquals(200, response.statusCode(), "There is error in the process of retrieving");
+        assertNotNull(response.getBody(), "Expected a non-null body");
+        List<WarehouseResponse> warehouses = response.jsonPath().getList("$", WarehouseResponse.class);
+        assertNotNull(warehouses, "The response is empty");
+
+        if (warehouses.isEmpty()) {
+            System.out.println("The list [] is expected in the first retrieval when no data exists in the database, before the warehouses are created.");
+        }
+
+        Set<String> warehouseIds = new HashSet<>();
+        Set<String> locations = new HashSet<>();
+
+        for (WarehouseResponse warehouse : warehouses) {
+            String warehouseId = warehouse.getWarehouseId();
+            String warehouselocation = warehouse.getLocation();
+
+            assertFalse(locations.contains(warehouselocation), "There is duplicate " + warehouselocation);
+            assertNotNull(warehouse.getCapacity(), "The mandatory field capacity should be filled");
+            assertNotNull(warehouse.getIsActive(), "The mandatory filed IsActive should be filled automatically");
+            assertNotNull(warehouse.getCurrentStock(), "The mandatory filed currentStock should be filled");
+            assertNotNull(warehouse.getWarehouseId(), "The filed warehouseId should be filled");
+
+            locations.add(warehouselocation);
+            warehouseIds.add(warehouseId);
+        }
+
+    }
 }
